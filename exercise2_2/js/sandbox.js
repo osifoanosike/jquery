7e280e81ed12b7bbf359e090732e3372fc580995
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-03-23 12:06:40 +0100

    Update code structure

--- exercise2_2/js/sandbox.js
+++ exercise2_2/js/sandbox.js
@@ -4,50 +4,6 @@ function Traversal() {
 
 Traversal.prototype = {
 
-  //question 2.1.1: Select all of the div elements that have a class of “module”.
-  selectModuleDivs: function() {
-    var result = $('.module');
-    console.log("All elements that have a class of 'module': ");
-    console.log(result);
-  },
-
-  /**Question 2.1.2
-    Come up with three selectors that you could use to get the third item in the #myList unordered list. 
-    Which is the best to use? Why?**/
-  selectListItems: function() {
-    //selector1
-    console.log("\nSelector 1: #myListItem");
-    var third_item_1 = $('#myListItem');
-    console.log(third_item_1);
-
-    //selector2
-    console.log("\nSelector 2: #myList li:nth-child(3)");
-    var third_item_2 = $('#myList li:nth-child(3)');
-    console.log(third_item_2);
-
-    //selector3
-    console.log("\nSelector 3: #myList li).get(2)");
-    var third_item_3 = $('#myList li').get(2);
-    console.log(third_item_3);
-
-    //best choice
-    console.log('My best option is using the list item id: #myListItem\n' +
-      'as DOM traversal is minimized and the list item with the specified unique id is directly targeted. \n' + 
-      'Also the returned result is a jquery function, thus further DOM operatins can be performed on it.');
-  },
-
-  //question 2.1 c: Select the label for the search input using an attribute selector.
-  selectFormLabel: function() {
-    var label = $('label[for=q]');
-    console.log('Label for search input: ' + label.text());
-  },
-
-  //question 2.1 d: Figure out how many elements on the page are hidden
-  hiddenElementCount: function() {
-    var count = $(':hidden').length;
-    console.log('Number of hidden input elements: ' + count);
-  },
-
   //question 2.2 a: Figure out how many image elements on the page have an alt attribute.
   log_img_attrs: function() {
     var $images = $('img'), count = 0;
