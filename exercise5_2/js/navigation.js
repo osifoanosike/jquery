369496243050b2e829418c3e47204fedd6153a9f
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-04-01 14:22:02 +0100

    Improve logic

--- exercise5_2/js/navigation.js
+++ exercise5_2/js/navigation.js
@@ -1,27 +1,16 @@
 function Navigation(menuItems){
-	this.menu_items = menuItems;
+  this.menuItems = menuItems;
 }
 
 Navigation.prototype = {
-	addEventHandler: function() {
-		$(this.menu_items).hover(
-			function(){
-				if($(this).has('ul')){
-					$(this).find('ul').show(300);
-				}
-			},
-
-			function(){
-				if($(this).has('ul')){
-					$(this).find('ul').hide(600);
-				}
-			}
-		);
-	}
+  addEventHandler: function() {
+    $(this.menuItems).hover(function() { 
+      $(this).toggleClass('hover').find('ul').toggle(400); });
+  }
 }
 
 $(document).ready(function(){
-	var $menuItems = $('ul#nav>li');
-	var navigation = new Navigation($menuItems);
-	navigation.addEventHandler();
+  var $menuItems = $('ul#nav>li');
+  var navigation = new Navigation($menuItems);
+  navigation.addEventHandler();
 });
