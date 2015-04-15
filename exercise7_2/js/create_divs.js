d46ec8a5aa5cb0375dec9ec7c684da1deb158b49
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-04-15 11:01:03 +0100

    Optimize code

--- exercise7_2/js/create_divs.js
+++ exercise7_2/js/create_divs.js
@@ -13,20 +13,15 @@ DivStack.prototype = {
   },
 
   createItemModel: function(){
-    this.model_stack_item = $('<div>', { 'class': 'stack_item' })
-      .css({ 'height':'2em', 'width': '75%', 'border':'1px solid #ccc',
-        'border-radius': '3px','margin': '5px', 'background-color':'#fafafa' });
+    this.model_stack_item = $('<div>', { 'class': 'stack_item' });
   },
 
   setupStack: function(){
     //create button
-    var $button = $('<button>', {'id':'addBtn', text: 'add item'})
-      .css({ 'position': 'absolute', 'bottom': '0', 'right': '0', 'margin': '5px' });
+    var $button = $('<button>', {'id':'addBtn', text: 'add item'});
 
     //create container
-    var $stack_container = $('<section>', { 'id': 'container' }).append($button)
-      .css({ 'width': '45%', 'margin': '0 auto', 'border': 'solid 1px #ccc',
-       'position': 'relative', 'min-height': '5em' });
+    var $stack_container = $('<section>', { 'id': 'container' }).append($button);
 
     //add UI elements to the DOM 
     $('body').append($($stack_container));
@@ -43,15 +38,20 @@ DivStack.prototype = {
       .prependTo(this.stack_container);
   },
 
+  clickStackItem: function(item) {
+    $('.selected').removeClass('selected');
+    $(item).addClass('selected');
+
+    if($(item).prevAll().length == 0 ){
+      $(item).fadeOut(400, function(){ $(this).remove(); });
+    }
+  },
+
   addEventHandler: function() {
     var that = this;
 
     this.stack_container.on('click', 'div.stack_item', function(){
-      if($(this).prevAll().length == 0 ){
-        $(this).fadeOut(400, function(){ $(this).remove(); });
-      } else {
-        $(this).css('border-color', '#ffa600');
-      }
+      that.clickStackItem(this);
     });
 
     this.addButton.on('click', function(){
